6b18cefef6d1a4dd10929f590cf51060ff7d8489
Aum Pauskar <aumpauskar111@gmail.com>
2025-12-28 10:12:32 +0530

    Added scroll support

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import subprocess
 gi.require_version("Gtk", "3.0")
 gi.require_version("AyatanaAppIndicator3", "0.1")
 
-from gi.repository import Gtk, AyatanaAppIndicator3 as AppIndicator3
+from gi.repository import Gtk, Gdk
+from gi.repository import AyatanaAppIndicator3 as AppIndicator3
 
 APPINDICATOR_ID = "brightness-control"
 
+current_brightness = 50
+
 def set_brightness(value):
+    global current_brightness
+    value = max(0, min(100, value))
+    current_brightness = value
+
     subprocess.run(
         ["ddcutil", "setvcp", "10", str(value)],
         stdout=subprocess.DEVNULL,
@@ -26,6 +33,9 @@ class BrightnessApp:
 
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.build_menu())
+
+        # 🔥 Enable scroll support
+        self.indicator.connect("scroll-event", self.on_scroll)
 
     def build_menu(self):
         menu = Gtk.Menu()
@@ -46,6 +56,14 @@ class BrightnessApp:
 
     def set_level(self, widget, level):
         set_brightness(level)
+
+    def on_scroll(self, indicator, delta, direction):
+        global current_brightness
+
+        if direction == Gdk.ScrollDirection.UP:
+            set_brightness(current_brightness + 5)
+        elif direction == Gdk.ScrollDirection.DOWN:
+            set_brightness(current_brightness - 5)
 
 if __name__ == "__main__":
     BrightnessApp()
